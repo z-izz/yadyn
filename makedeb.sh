a99a0d21f8b2a4291425b7157b3c9dab9e575a8b
Winksplorer <winksplorer@gordae.com>
2023-07-14 23:34:19 -0700

    more debug info in makedeb.sh

--- makedeb.sh
+++ makedeb.sh
@@ -3,6 +3,7 @@
 # make sure we are sudo
 sudo echo "Building deb..."
 
+echo "Creating directories..."
 # make directories
 mkdir yadyn
 mkdir yadyn/DEBIAN
@@ -11,6 +12,7 @@ mkdir yadyn/usr/bin
 mkdir yadyn/usr/share
 mkdir yadyn/usr/share/yadyn
 
+echo "Creating DEBIAN/control file..."
 cat <<EOF > yadyn/DEBIAN/control
 Package: yadyn
 Version: 1.1.0
@@ -23,17 +25,21 @@ Description: Dynamic wallpaper manager for Linux
 Homepage: https://github.com/z-izz/yadyn/
 EOF
 
+echo "Adding the runner file..."
 # add the runner file
 cp runner.sh yadyn/usr/bin/yadyn
 
+echo "Adding the ruby files..."
 # add the ruby files
 cp *.rb yadyn/usr/share/yadyn
 
+echo "Changing permissions for the deb..."
 # give root permissions to the dir
 sudo chown -R root:root yadyn
 sudo chmod 0755 yadyn/DEBIAN
 sudo chmod 0755 yadyn/usr/bin/*
 sudo chmod 0755 yadyn/usr/share/yadyn*
 
+echo "Packing deb..."
 # build the deb
 sudo dpkg-deb --build yadyn
